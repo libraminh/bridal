fbd5620082cc58f6c1961bfe0470f85a6951a929
libraminh <libraminh@gmail.com>
2017-07-26 14:44:42 +0700

    ...

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,8 +1,139 @@
 $(document).ready(function() {
   scrollPage();
   scrollReveal();
+  playCarousel();
+  showPopover();
+  activeColorCarousel();
+  openCart();
+  numberButton();
 
 })
+
+function numberButton() {
+  jQuery('<div class="quantity-nav"><div class="quantity-button quantity-up"><i class="fa fa-angle-up" aria-hidden="true"></i></div><div class="quantity-button quantity-down"><i class="fa fa-angle-down" aria-hidden="true"></i></div></div>').insertAfter('.quantity input');
+  jQuery('.quantity').each(function() {
+    var spinner = jQuery(this),
+      input = spinner.find('input[type="number"]'),
+      btnUp = spinner.find('.quantity-up'),
+      btnDown = spinner.find('.quantity-down'),
+      min = input.attr('min'),
+      max = input.attr('max');
+
+    btnUp.click(function() {
+      var oldValue = parseFloat(input.val());
+      if (oldValue >= max) {
+        var newVal = oldValue;
+      } else {
+        var newVal = oldValue + 1;
+      }
+      spinner.find("input").val(newVal);
+      spinner.find("input").trigger("change");
+    });
+
+    btnDown.click(function() {
+      var oldValue = parseFloat(input.val());
+      if (oldValue <= min) {
+        var newVal = oldValue;
+      } else {
+        var newVal = oldValue - 1;
+      }
+      spinner.find("input").val(newVal);
+      spinner.find("input").trigger("change");
+    });
+
+  });
+}
+
+function openCart() {
+  var linkCart = $('.main-nav #shoppingmenu'),
+      cart = $('.main-nav #shoppingmenu #Capa_1'),
+      badge = $('.main-nav #shoppingmenu .badge')
+
+  $(linkCart).on('click', function(){
+    $(cart).toggleClass('open-cart');
+    $(badge).toggleClass('open-cart');
+  })
+}
+
+function activeColorCarousel() {
+  var pic = $('.carousel-4 img, .carousel-2 img');
+
+  $(pic).on('click', function(){
+    $(this).addClass('active-color');
+    $(this).parent().parent().siblings().find('img').removeClass('active-color');
+  })
+}
+
+function showPopover() {
+  $('#optionsRadios2').popover({
+    'placement' : 'top'
+  })
+}
+
+function playCarousel() {
+  if($('body').is('.product-detail')) {
+
+    $('.carousel-3').owlCarousel({
+          items: 4,
+          margin: 40,
+          loop: true,
+          nav: true,
+          navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+          responsive:{
+            0:{
+                items:2,
+            },
+            768:{
+                margin: 20,
+                items:2,
+            },
+            1000:{
+                items:3,
+                dots: false,
+            },
+          }
+      });
+
+    $('.carousel-2').owlCarousel({
+          margin: 10,
+          nav: true,
+          dots: false,
+          navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+          responsive:{
+            0:{
+                items:5,
+            },
+            768:{
+                margin: 10,
+                items:4,
+            },
+            1000:{
+                items:4,
+            },
+          }
+      });
+
+    $('.carousel-1').owlCarousel({
+      items:1,
+      loop:false,
+      margin:10,
+      URLhashListener:true,
+      autoplayHoverPause:true,
+      startPosition: 'URLHash',
+      dots: false,
+    });
+
+    $('.carousel-4').owlCarousel({
+      items:6,
+      loop:false,
+      margin:10,
+      URLhashListener:true,
+      autoplayHoverPause:true,
+      startPosition: 'URLHash'
+    });
+
+  }
+}
 
 $(window).scroll(function(){
       changeTextColor();
